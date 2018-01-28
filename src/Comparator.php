60dca773b0528d788e256442f1534818f5fe88c1
James Titcumb <james@asgrim.com>
2018-01-28 11:35:47 +0100

    Split out into multiple private focused methods

--- src/Comparator.php
+++ src/Comparator.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace Roave\ApiCompare;
 
+use Roave\BetterReflection\Reflection\ReflectionClass;
+use Roave\BetterReflection\Reflection\ReflectionMethod;
+use Roave\BetterReflection\Reflection\ReflectionParameter;
 use Roave\BetterReflection\Reflector\ClassReflector;
 use Roave\BetterReflection\Reflector\Exception\IdentifierNotFound;
 
@@ -13,45 +16,75 @@ final class Comparator
         $changelog = [];
 
         foreach ($oldApi->getAllClasses() as $oldClass) {
-            try {
-                $newClass = $newApi->reflect($oldClass->getName());
-            } catch (IdentifierNotFound $exception) {
-                $changelog[] = sprintf('[BC] Class %s has been deleted', $oldClass->getName());
-                continue;
-            }
-
-            foreach ($oldClass->getMethods() as $oldMethod) {
-                // @todo ignore private methods
-                try {
-                    $newMethod = $newClass->getMethod($oldMethod->getName());
-                } catch (\OutOfBoundsException $exception) {
-                    $changelog[] = sprintf(
-                        '[BC] Method %s in class %s has been deleted',
-                        $oldMethod->getName(),
-                        $oldClass->getName()
-                    );
-                    continue;
-                }
-
-                foreach ($oldMethod->getParameters() as $oldParameter) {
-                    $newParameter = $newMethod->getParameter($oldParameter->getName());
-
-                    if (null === $newParameter) {
-                        $changelog[] = sprintf(
-                            '[BC] Parameter %s in %s%s%s has been deleted',
-                            $oldParameter->getName(),
-                            $oldClass->getName(),
-                            $oldMethod->isStatic() ? '#' : '::',
-                            $oldMethod->getName()
-                        );
-                        continue;
-                    }
-
-                    // @todo check if types changed, or becoming default
-                    // @todo check if a new param (without a default) was added
-                }
-            }
+            $changelog = $this->examineClass($changelog, $oldClass, $newApi);
         }
+
+        return $changelog;
+    }
+
+    private function examineClass(array $changelog, ReflectionClass $oldClass, ClassReflector $newApi): array
+    {
+        try {
+            $newClass = $newApi->reflect($oldClass->getName());
+        } catch (IdentifierNotFound $exception) {
+            $changelog[] = sprintf('[BC] Class %s has been deleted', $oldClass->getName());
+            return $changelog;
+        }
+
+        foreach ($oldClass->getMethods() as $oldMethod) {
+            $changelog = $this->examineMethod($changelog, $oldClass, $oldMethod, $newClass);
+        }
+
+        return $changelog;
+    }
+
+    private function examineMethod(
+        array $changelog,
+        ReflectionClass $oldClass,
+        ReflectionMethod $oldMethod,
+        ReflectionClass $newClass
+    ): array {
+        // @todo ignore private methods
+        try {
+            $newMethod = $newClass->getMethod($oldMethod->getName());
+        } catch (\OutOfBoundsException $exception) {
+            $changelog[] = sprintf(
+                '[BC] Method %s in class %s has been deleted',
+                $oldMethod->getName(),
+                $oldClass->getName()
+            );
+            return $changelog;
+        }
+
+        foreach ($oldMethod->getParameters() as $oldParameter) {
+            $changelog = $this->examineParameter($changelog, $oldClass, $oldMethod, $oldParameter, $newMethod);
+        }
+
+        return $changelog;
+    }
+
+    private function examineParameter(
+        array $changelog,
+        ReflectionClass $oldClass,
+        ReflectionMethod $oldMethod,
+        ReflectionParameter $oldParameter,
+        ReflectionMethod $newMethod
+    ): array {
+        $newParameter = $newMethod->getParameter($oldParameter->getName());
+
+        if (null === $newParameter) {
+            $changelog[] = sprintf(
+                '[BC] Parameter %s in %s%s%s has been deleted',
+                $oldParameter->getName(),
+                $oldClass->getName(),
+                $oldMethod->isStatic() ? '#' : '::',
+                $oldMethod->getName()
+            );
+            return $changelog;
+        }
+
+        // @todo check if types changed, or becoming default
+        // @todo check if a new param (without a default) was added
 
         return $changelog;
     }
