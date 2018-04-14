a87e1cf7d8613e4c4b8465fc080e8953f2626d4b
Marco Pivetta <ocramius@gmail.com>
2018-04-14 17:37:39 +0200

    #15 run CLI in an IIFE, remove unused variables

--- bin/api-compare.php
+++ bin/api-compare.php
@@ -1,31 +1,37 @@
 <?php
+
 declare(strict_types=1);
 
-use Roave\ApiCompare\Command;
-use Symfony\Component\Console\Application;
+namespace Roave\ApiCompareCli;
 
-$foundAutoload = false;
-foreach ([__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../autoload.php'] as $autoload) {
-    if (file_exists($autoload)) {
+use Roave\ApiCompare\Command;
+use RuntimeException;
+use Symfony\Component\Console\Application;
+use function file_exists;
+
+(function () {
+    foreach ([__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../autoload.php'] as $autoload) {
+        if (! file_exists($autoload)) {
+            continue;
+        }
+
         /** @noinspection PhpIncludeInspection */
         require $autoload;
-        $foundAutoload = true;
-        break;
+
+        $apiCompareCommand = new Command\ApiCompare(
+            new \Roave\ApiCompare\Git\GitCheckoutRevisionToTemporaryPath(),
+            new \Roave\ApiCompare\Factory\DirectoryReflectorFactory()
+        );
+
+        $application = new Application();
+        $application->add($apiCompareCommand);
+        $application->setDefaultCommand($apiCompareCommand->getName());
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $application->run();
+
+        return;
     }
-}
 
-if (!$foundAutoload) {
-    throw new \RuntimeException('Could not find Composer autoload.php');
-}
-
-$apiCompareCommand = new Command\ApiCompare(
-    new \Roave\ApiCompare\Git\GitCheckoutRevisionToTemporaryPath(),
-    new \Roave\ApiCompare\Factory\DirectoryReflectorFactory()
-);
-
-$application = new Application();
-$application->add($apiCompareCommand);
-$application->setDefaultCommand($apiCompareCommand->getName());
-
-/** @noinspection PhpUnhandledExceptionInspection */
-$application->run();
+    throw new RuntimeException('Could not find Composer autoload.php');
+})();
