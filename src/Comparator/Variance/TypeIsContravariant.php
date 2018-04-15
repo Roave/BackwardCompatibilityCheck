42def53f299c2ddf16b4f76c8c183a87e8b1572a
Marco Pivetta <ocramius@gmail.com>
2018-04-15 21:19:09 +0200

    Adding an utility to check if covariance is being respected in a type change

---
+++ src/Comparator/Variance/TypeIsContravariant.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\ApiCompare\Comparator\Variance;
+
+use Roave\BetterReflection\Reflection\ReflectionType;
+use Roave\BetterReflection\Reflector\ClassReflector;
+
+/**
+ * This is a simplistic contravariant type check. A more appropriate approach would be to
+ * have a `$type->includes($otherType)` check with actual types represented as value objects,
+ * but that is a massive piece of work that should be done by importing an external library
+ * instead, if this class no longer suffices.
+ */
+final class TypeIsContravariant
+{
+    public function __invoke(
+        ClassReflector $reflector,
+        ?ReflectionType $type,
+        ?ReflectionType $comparedType
+    ) : bool {
+        if ($comparedType === null) {
+            return true;
+        }
+
+        if ($type === null) {
+            // nothing can be contravariant to `mixed` besides `mixed` itself (handled above)
+            return false;
+        }
+
+        $typeAsString         = $type->__toString();
+        $comparedTypeAsString = $comparedType->__toString();
+
+        if (strtolower($typeAsString) === strtolower($comparedTypeAsString)) {
+            return true;
+        }
+
+        if (strtolower($typeAsString) === 'void') {
+            // everything is always contravariant to `void`
+            return true;
+        }
+
+        if (strtolower($comparedTypeAsString) === 'object' && ! $type->isBuiltin()) {
+            // `object` is always contravariant to any object type
+            return true;
+        }
+
+        if (strtolower($comparedTypeAsString) === 'iterable') {
+            if (strtolower($typeAsString) === 'array') {
+                return true;
+            }
+        }
+
+        if (strtolower($typeAsString) === 'iterable') {
+            if (! $comparedType->isBuiltin()) {
+                $comparedTypeReflectionClass = $reflector->reflect($comparedTypeAsString);
+
+                if ($comparedTypeReflectionClass->implementsInterface(\Traversable::class)) {
+                    // `iterable` can be replaced by any `Iterator` implementation
+                    return true;
+                }
+            }
+        }
+
+        if ($type->isBuiltin() !== $comparedType->isBuiltin()) {
+            return false;
+        }
+
+        if ($type->isBuiltin()) {
+            // All other type declarations have no variance/contravariance relationship
+            return false;
+        }
+
+        $typeReflectionClass = $reflector->reflect($typeAsString);
+        $comparedTypeReflectionClass = $reflector->reflect($comparedTypeAsString);
+
+        if ($comparedTypeReflectionClass->isInterface()) {
+            return $typeReflectionClass->implementsInterface($comparedTypeAsString);
+        }
+
+        return in_array($comparedTypeAsString, $typeReflectionClass->getParentClassNames(), true);
+    }
+}
---
+++ test/unit/Comparator/Variance/TypeIsContravariantTest.php
@@ -0,0 +1,233 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RoaveTest\ApiCompare\Comparator\BackwardsCompatibility\ClassBased;
+
+use PHPUnit\Framework\TestCase;
+use Roave\ApiCompare\Comparator\Variance\TypeIsContravariant;
+use Roave\BetterReflection\BetterReflection;
+use Roave\BetterReflection\Reflection\ReflectionType;
+use Roave\BetterReflection\Reflector\ClassReflector;
+use Roave\BetterReflection\SourceLocator\Type\StringSourceLocator;
+
+final class TypeIsContravariantTest extends TestCase
+{
+    /** @dataProvider checkedTypes */
+    public function testContravariance(
+        ?ReflectionType $type,
+        ?ReflectionType $newType,
+        bool $expectedToBeContravariant
+    ) : void {
+        $reflector = new ClassReflector(new StringSourceLocator(
+            <<<'PHP'
+<?php
+
+interface Traversable {}
+interface Iterator extends Traversable {}
+interface AnInterface {}
+interface AnotherInterface {}
+class AnotherClass implements AnInterface {}
+class AnotherClassWithMultipleInterfaces implements AnInterface, AnotherInterface {}
+class AClass {}
+class BClass extends AClass {}
+class CClass extends BClass {}
+PHP
+            ,
+            (new BetterReflection())->astLocator()
+        ));
+
+        self::assertSame(
+            $expectedToBeContravariant,
+            (new TypeIsContravariant())
+                ->__invoke($reflector, $type, $newType)
+        );
+    }
+
+    public function checkedTypes() : array
+    {
+        return [
+            'no type to no type is contravariant with itself'                          => [
+                null,
+                null,
+                true,
+            ],
+            'no type to void type is not contravariant'                                => [
+                null,
+                ReflectionType::createFromType('void', false),
+                false,
+            ],
+            'void type to no type is contravariant'                                    => [
+                ReflectionType::createFromType('void', false),
+                null,
+                true,
+            ],
+            'void type to scalar type is contravariant'                                => [
+                ReflectionType::createFromType('void', false),
+                ReflectionType::createFromType('string', false),
+                true,
+            ],
+            'void type to class type is contravariant'                                 => [
+                ReflectionType::createFromType('void', false),
+                ReflectionType::createFromType('AClass', false),
+                true,
+            ],
+            'scalar type to no type is contravariant'                                  => [
+                ReflectionType::createFromType('string', false),
+                null,
+                true,
+            ],
+            'no type to scalar type is not contravariant'                              => [
+                null,
+                ReflectionType::createFromType('string', false),
+                false,
+            ],
+            'class type to no type is contravariant'                                   => [
+                ReflectionType::createFromType('AClass', false),
+                null,
+                true,
+            ],
+            'no type to class type is not contravariant'                               => [
+                ReflectionType::createFromType('AClass', false),
+                null,
+                true,
+            ],
+            'iterable to non-iterable class type is not contravariant'                 => [
+                ReflectionType::createFromType('iterable', false),
+                ReflectionType::createFromType('AnotherClassWithMultipleInterfaces', false),
+                false,
+            ],
+            'iterable to iterable class type is contravariant'                         => [
+                ReflectionType::createFromType('iterable', false),
+                ReflectionType::createFromType('Iterator', false),
+                true,
+            ],
+            'non-iterable class to iterable type is not contravariant'                 => [
+                ReflectionType::createFromType('iterable', false),
+                ReflectionType::createFromType('AnotherClassWithMultipleInterfaces', false),
+                false,
+            ],
+            'iterable class type to iterable is not contravariant'                     => [
+                ReflectionType::createFromType('Iterator', false),
+                ReflectionType::createFromType('iterable', false),
+                false,
+            ],
+            'object to class type is not contravariant'                                => [
+                ReflectionType::createFromType('object', false),
+                ReflectionType::createFromType('AClass', false),
+                false,
+            ],
+            'class type to object is contravariant'                                    => [
+                ReflectionType::createFromType('AClass', false),
+                ReflectionType::createFromType('object', false),
+                true,
+            ],
+            'class type to scalar type is not contravariant'                           => [
+                ReflectionType::createFromType('AClass', false),
+                ReflectionType::createFromType('string', false),
+                false,
+            ],
+            'scalar type to class type is not contravariant'                           => [
+                ReflectionType::createFromType('string', false),
+                ReflectionType::createFromType('AClass', false),
+                false,
+            ],
+            'scalar type (string) to different scalar type (int) is not contravariant' => [
+                ReflectionType::createFromType('string', false),
+                ReflectionType::createFromType('int', false),
+                false,
+            ],
+            'scalar type (int) to different scalar type (float) is not contravariant'  => [
+                ReflectionType::createFromType('int', false),
+                ReflectionType::createFromType('float', false),
+                false,
+            ],
+            'object type to scalar type is not contravariant'                          => [
+                ReflectionType::createFromType('object', false),
+                ReflectionType::createFromType('string', false),
+                false,
+            ],
+            'scalar type to object type is not contravariant'                          => [
+                ReflectionType::createFromType('string', false),
+                ReflectionType::createFromType('object', false),
+                false,
+            ],
+            'class to superclass is contravariant'                                     => [
+                ReflectionType::createFromType('BClass', false),
+                ReflectionType::createFromType('AClass', false),
+                true,
+            ],
+            'class to subclass is not contravariant'                                   => [
+                ReflectionType::createFromType('BClass', false),
+                ReflectionType::createFromType('CClass', false),
+                false,
+            ],
+            'class to implemented interface is contravariant'                          => [
+                ReflectionType::createFromType('AnotherClassWithMultipleInterfaces', false),
+                ReflectionType::createFromType('AnInterface', false),
+                true,
+            ],
+            'class to not implemented interface is not contravariant'                  => [
+                ReflectionType::createFromType('AnotherClassWithMultipleInterfaces', false),
+                ReflectionType::createFromType('Traversable', false),
+                false,
+            ],
+            'interface to parent interface is contravariant'                           => [
+                ReflectionType::createFromType('Iterator', false),
+                ReflectionType::createFromType('Traversable', false),
+                true,
+            ],
+            'interface to child interface is contravariant'                            => [
+                ReflectionType::createFromType('Traversable', false),
+                ReflectionType::createFromType('Iterator', false),
+                false,
+            ],
+        ];
+    }
+
+    /** @dataProvider existingTypes */
+    public function testContravarianceConsidersSameTypeAlwaysContravariant(?ReflectionType $type) : void
+    {
+        $reflector = new ClassReflector(new StringSourceLocator(
+            <<<'PHP'
+<?php
+
+interface Traversable {}
+class AClass {}
+PHP
+            ,
+            (new BetterReflection())->astLocator()
+        ));
+
+        self::assertTrue(
+            (new TypeIsContravariant())
+                ->__invoke($reflector, $type, $type)
+        );
+    }
+
+    public function existingTypes() : array
+    {
+        return array_merge(
+            [[null]],
+            array_merge(...array_map(
+                function (string $type) : array {
+                    return [
+                        [ReflectionType::createFromType($type, false)],
+                        [ReflectionType::createFromType($type, true)],
+                    ];
+                },
+                [
+                    'int',
+                    'string',
+                    'float',
+                    'bool',
+                    'array',
+                    'iterable',
+                    'callable',
+                    'Traversable',
+                    'AClass',
+                ]
+            ))
+        );
+    }
+}
