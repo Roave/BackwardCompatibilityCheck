44c1946cceae601b2b4685e6eb4bc177c8b1ac0e
Marco Pivetta <ocramius@gmail.com>
2018-04-15 18:14:11 +0200

    #33 clarified comment about visibility (as string) being sortable as per @akrabat's review

--- src/Comparator/BackwardsCompatibility/ClassBased/PropertyVisibilityReduced.php
+++ src/Comparator/BackwardsCompatibility/ClassBased/PropertyVisibilityReduced.php
@@ -37,7 +37,7 @@ final class PropertyVisibilityReduced implements ClassBased
                 )
             ),
             function (array $visibilities) : bool {
-                // Note: works because public, protected and private are (luckily) sortable
+                // Note: works because the strings "public", "protected" and "private" can be compared directly as they sort correctly for our purposes
                 return $visibilities[0] > $visibilities[1];
             }
         );
