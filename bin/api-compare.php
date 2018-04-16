b583e962ad3db9c0e0c0945691fe34ca23295fb1
Marco Pivetta <ocramius@gmail.com>
2018-04-17 00:32:15 +0200

    Wiring up the comparator! All aboard! \o/

--- bin/api-compare.php
+++ bin/api-compare.php
@@ -6,6 +6,14 @@ namespace Roave\ApiCompareCli;
 
 use Roave\ApiCompare\Command;
 use Roave\ApiCompare\Comparator;
+use Roave\ApiCompare\Comparator\BackwardsCompatibility\ClassBased;
+use Roave\ApiCompare\Comparator\BackwardsCompatibility\ClassConstantBased;
+use Roave\ApiCompare\Comparator\BackwardsCompatibility\FunctionBased;
+use Roave\ApiCompare\Comparator\BackwardsCompatibility\InterfaceBased;
+use Roave\ApiCompare\Comparator\BackwardsCompatibility\MethodBased;
+use Roave\ApiCompare\Comparator\BackwardsCompatibility\PropertyBased;
+use Roave\ApiCompare\Comparator\Variance\TypeIsContravariant;
+use Roave\ApiCompare\Comparator\Variance\TypeIsCovariant;
 use Roave\ApiCompare\Factory\DirectoryReflectorFactory;
 use Roave\ApiCompare\Git\GetVersionCollectionFromGitRepository;
 use Roave\ApiCompare\Git\GitCheckoutRevisionToTemporaryPath;
@@ -31,8 +39,37 @@ use function file_exists;
             new GetVersionCollectionFromGitRepository(),
             new PickLastMinorVersionFromCollection(),
             new Comparator(
-                new Comparator\BackwardsCompatibility\ClassBased\PropertyRemoved(),
-                new Comparator\BackwardsCompatibility\FunctionBased\ParameterDefaultValueChanged()
+                new ClassBased\MultiClassBased(
+                    new ClassBased\ClassBecameFinal(),
+                    new ClassBased\ConstantRemoved(),
+                    new ClassBased\PropertyRemoved(),
+                    new ClassBased\MethodRemoved()
+                ),
+                new InterfaceBased\MethodAdded(),
+                new MethodBased\MultiMethodBased(
+                    new MethodBased\MethodScopeChanged(),
+                    new MethodBased\MethodVisibilityReduced(),
+                    new MethodBased\AccessibleMethodFunctionBasedChange(
+                        new FunctionBased\MultiFunctionBased(
+                            new FunctionBased\ParameterByReferenceChanged(),
+                            new FunctionBased\ReturnTypeByReferenceChanged(),
+                            new FunctionBased\RequiredParameterAmountIncreased(),
+                            new FunctionBased\ParameterDefaultValueChanged(),
+                            new FunctionBased\ReturnTypeCovarianceChanged(new TypeIsCovariant()),
+                            new FunctionBased\ParameterTypeContravarianceChanged(new TypeIsContravariant())
+                        )
+                    )
+                ),
+                new PropertyBased\MultiPropertyBased(
+                    new PropertyBased\PropertyDocumentedTypeChanged(),
+                    new PropertyBased\PropertyDefaultValueChanged(),
+                    new PropertyBased\PropertyVisibilityReduced(),
+                    new PropertyBased\PropertyScopeChanged()
+                ),
+                new ClassConstantBased\MultiConstantBased(
+                    new ClassConstantBased\ConstantVisibilityReduced(),
+                    new ClassConstantBased\ConstantValueChanged()
+                )
             )
         );
 
