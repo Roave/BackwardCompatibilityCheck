0d33cd07db48aaaa198ff32e2a33429cf193c88d
Marco Pivetta <ocramius@gmail.com>
2018-04-16 23:25:45 +0200

    Adding checker for constant visibility reduction - prevents decreasing visibility of a class constant

---
+++ src/Comparator/BackwardsCompatibility/ConstantBased/ConstantVisibilityReduced.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\ApiCompare\Comparator\BackwardsCompatibility\ConstantBased;
+
+use Roave\ApiCompare\Change;
+use Roave\ApiCompare\Changes;
+use Roave\BetterReflection\Reflection\ReflectionClassConstant;
+
+final class ConstantVisibilityReduced implements ConstantBased
+{
+    private const VISIBILITY_PRIVATE = 'private';
+
+    private const VISIBILITY_PROTECTED = 'protected';
+
+    private const VISIBILITY_PUBLIC = 'public';
+
+    public function compare(ReflectionClassConstant $fromConstant, ReflectionClassConstant $toConstant) : Changes
+    {
+        $visibilityFrom = $this->propertyVisibility($fromConstant);
+        $visibilityTo   = $this->propertyVisibility($toConstant);
+
+        if ($visibilityFrom <= $visibilityTo) {
+            return Changes::new();
+        }
+
+        return Changes::fromArray([Change::changed(
+            sprintf(
+                'Constant %s::%s visibility reduced from %s to %s',
+                $fromConstant->getDeclaringClass()->getName(),
+                $fromConstant->getName(),
+                $visibilityFrom,
+                $visibilityTo
+            ),
+            true
+        )]);
+    }
+
+    private function propertyVisibility(ReflectionClassConstant $property) : string
+    {
+        if ($property->isPublic()) {
+            return self::VISIBILITY_PUBLIC;
+        }
+
+        if ($property->isProtected()) {
+            return self::VISIBILITY_PROTECTED;
+        }
+
+        return self::VISIBILITY_PRIVATE;
+    }
+}
