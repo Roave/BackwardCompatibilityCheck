6816eadbb5b0383863d8cd7afa7d8403c8774201
Marco Pivetta <ocramius@gmail.com>
2018-04-16 21:05:35 +0200

    BC compliance check that verifies is a class became `final`

---
+++ src/Comparator/BackwardsCompatibility/ClassBased/ClassBecameFinal.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\ApiCompare\Comparator\BackwardsCompatibility\ClassBased;
+
+use Assert\Assert;
+use Roave\ApiCompare\Change;
+use Roave\ApiCompare\Changes;
+use Roave\BetterReflection\Reflection\ReflectionClass;
+use Roave\BetterReflection\Reflection\ReflectionClassConstant;
+
+final class ClassBecameFinal implements ClassBased
+{
+    public function compare(ReflectionClass $fromClass, ReflectionClass $toClass) : Changes
+    {
+        Assert::that($fromClass->getName())->same($toClass->getName());
+
+        if ($fromClass->isFinal()) {
+            return Changes::new();
+        }
+
+        if (! $toClass->isFinal()) {
+            return Changes::new();
+        }
+
+        return Changes::fromArray([Change::changed(
+            sprintf('Class %s became final', $fromClass->getName()),
+            true
+        )]);
+    }
+}
