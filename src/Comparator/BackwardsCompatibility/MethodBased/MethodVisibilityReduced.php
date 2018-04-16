5c4ce73ba92db98c0caee790720e1d6b54e482cb
Marco Pivetta <ocramius@gmail.com>
2018-04-16 23:40:20 +0200

    Moved method visibility reduction checks to their own `MethodBased` namespace

---
+++ src/Comparator/BackwardsCompatibility/MethodBased/MethodVisibilityReduced.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\ApiCompare\Comparator\BackwardsCompatibility\MethodBased;
+
+use Roave\ApiCompare\Change;
+use Roave\ApiCompare\Changes;
+use Roave\BetterReflection\Reflection\ReflectionClassConstant;
+use Roave\BetterReflection\Reflection\ReflectionMethod;
+
+final class MethodVisibilityReduced implements MethodBased
+{
+    private const VISIBILITY_PRIVATE = 'private';
+
+    private const VISIBILITY_PROTECTED = 'protected';
+
+    private const VISIBILITY_PUBLIC = 'public';
+
+    public function compare(ReflectionMethod $fromMethod, ReflectionMethod $toMethod) : Changes
+    {
+        $visibilityFrom = $this->methodVisibility($fromMethod);
+        $visibilityTo   = $this->methodVisibility($toMethod);
+
+        if ($visibilityFrom <= $visibilityTo) {
+            return Changes::new();
+        }
+
+        return Changes::fromArray([Change::changed(
+            sprintf(
+                'Method %s() of class %s visibility reduced from %s to %s',
+                $fromMethod->getName(),
+                $fromMethod->getDeclaringClass()->getName(),
+                $visibilityFrom,
+                $visibilityTo
+            ),
+            true
+        )]);
+    }
+
+    private function methodVisibility(ReflectionMethod $method) : string
+    {
+        if ($method->isPublic()) {
+            return self::VISIBILITY_PUBLIC;
+        }
+
+        if ($method->isProtected()) {
+            return self::VISIBILITY_PROTECTED;
+        }
+
+        return self::VISIBILITY_PRIVATE;
+    }
+}
