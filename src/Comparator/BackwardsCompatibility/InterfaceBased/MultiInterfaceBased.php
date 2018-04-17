dcb4d2c730dce1cfff1c09c0b5a4328089b46301
Marco Pivetta <ocramius@gmail.com>
2018-04-17 15:45:27 +0200

    Adding a way to check multiple interface changes at once

---
+++ src/Comparator/BackwardsCompatibility/InterfaceBased/MultiInterfaceBased.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\ApiCompare\Comparator\BackwardsCompatibility\InterfaceBased;
+
+use Roave\ApiCompare\Changes;
+use Roave\BetterReflection\Reflection\ReflectionClass;
+use function array_reduce;
+
+final class MultiInterfaceBased implements InterfaceBased
+{
+    /** @var InterfaceBased[] */
+    private $checks;
+
+    public function __construct(InterfaceBased ...$checks)
+    {
+        $this->checks = $checks;
+    }
+
+    public function compare(ReflectionClass $fromClass, ReflectionClass $toClass) : Changes
+    {
+        return array_reduce(
+            $this->checks,
+            function (Changes $changes, InterfaceBased $check) use ($fromClass, $toClass) : Changes {
+                return $changes->mergeWith($check->compare($fromClass, $toClass));
+            },
+            Changes::new()
+        );
+    }
+}
