e1a015e63316098048b5ef147c7fb89db74e6e17
Marco Pivetta <ocramius@gmail.com>
2018-04-18 16:17:37 +0200

    Utilities to verify open/closed class changes

---
+++ src/Comparator/BackwardsCompatibility/ClassBased/FinalClassChanged.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\ApiCompare\Comparator\BackwardsCompatibility\ClassBased;
+
+use Roave\ApiCompare\Changes;
+use Roave\BetterReflection\Reflection\ReflectionClass;
+
+final class FinalClassChanged implements ClassBased
+{
+    /** @var ClassBased */
+    private $checkClass;
+
+    public function __construct(ClassBased $checkClass)
+    {
+        $this->checkClass = $checkClass;
+    }
+
+    public function compare(ReflectionClass $fromClass, ReflectionClass $toClass) : Changes
+    {
+        if (! $fromClass->isFinal()) {
+            return Changes::new();
+        }
+
+        return $this->checkClass->compare($fromClass, $toClass);
+    }
+}
