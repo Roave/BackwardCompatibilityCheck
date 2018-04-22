33be9dcb45d856d8c22757fd2cde0d2f8aa08621
Marco Pivetta <ocramius@gmail.com>
2018-04-22 12:05:53 +0200

    Added filters to check only public/protected methods

---
+++ src/Comparator/BackwardsCompatibility/MethodBased/OnlyPublicMethodChange.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\ApiCompare\Comparator\BackwardsCompatibility\MethodBased;
+
+use Roave\ApiCompare\Changes;
+use Roave\BetterReflection\Reflection\ReflectionMethod;
+
+/**
+ * Performs a method BC compliance check on methods that are public
+ */
+final class OnlyPublicMethodChange implements MethodBased
+{
+    /** @var MethodBased */
+    private $check;
+
+    public function __construct(MethodBased $check)
+    {
+        $this->check = $check;
+    }
+
+    public function compare(ReflectionMethod $fromMethod, ReflectionMethod $toMethod) : Changes
+    {
+        if (! $fromMethod->isPublic()) {
+            return Changes::new();
+        }
+
+        return $this->check->compare($fromMethod, $toMethod);
+    }
+}
