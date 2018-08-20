72f6201a089a685ae6e787cad102d4d751dcde04
Marco Pivetta <ocramius@gmail.com>
2018-08-20 19:31:07 +0200

    #85 added a way to skip errors when exceptions are raised during class diff analysis

---
+++ src/DetectChanges/BCBreak/ClassBased/SkipClassBasedErrors.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\BackwardCompatibility\DetectChanges\BCBreak\ClassBased;
+
+use Roave\BackwardCompatibility\Change;
+use Roave\BackwardCompatibility\Changes;
+use Roave\BetterReflection\Reflection\ReflectionClass;
+use Throwable;
+
+final class SkipClassBasedErrors implements ClassBased
+{
+    /** @var ClassBased */
+    private $next;
+
+    public function __construct(ClassBased $next)
+    {
+        $this->next = $next;
+    }
+
+    public function __invoke(ReflectionClass $fromClass, ReflectionClass $toClass) : Changes
+    {
+        try {
+            return $this->next->__invoke($fromClass, $toClass);
+        } catch (Throwable $failure) {
+            return Changes::fromList(Change::skippedDueToFailure($failure));
+        }
+    }
+}
