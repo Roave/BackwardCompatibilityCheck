ddaa280b7295d61e8065a548d44fe558a93b29f7
Marco Pivetta <ocramius@gmail.com>
2019-04-23 18:19:10 +0200

    Verify that non-directories are rejected in `CheckedOutRepository`

--- test/unit/Git/CheckedOutRepositoryTest.php
+++ test/unit/Git/CheckedOutRepositoryTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace RoaveTest\BackwardCompatibility\Git;
 
+use Assert\AssertionFailedException;
 use PHPUnit\Framework\TestCase;
 use Roave\BackwardCompatibility\Git\CheckedOutRepository;
 use function Safe\mkdir;
@@ -27,5 +28,12 @@ final class CheckedOutRepositoryTest extends TestCase
 
         rmdir($path . '/.git');
         rmdir($path);
+    }
+
+    public function testFromPathRejectsNonExistingDirectory() : void
+    {
+        $this->expectException(AssertionFailedException::class);
+
+        CheckedOutRepository::fromPath(__DIR__ . '/non-existing');
     }
 }
