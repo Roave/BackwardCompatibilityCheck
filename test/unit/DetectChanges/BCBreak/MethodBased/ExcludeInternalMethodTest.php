73b9459c8d2ae828a4e82082b951b7237bd0c5f7
Marco Pivetta <ocramius@gmail.com>
2019-07-17 09:04:47 +0200

    #51 allow skipping BC checks on internal methods
    While this is very similar to skipping BC checks on internal functions,
    we need to repeat the declarations due to the monomorphic nature of PHP
    type declarations. If we started to declare Class-alike checks that allow
    for `ClassBased|InterfaceBased|TraitBased` types to be given in, then this
    may be fixed.

---
+++ test/unit/DetectChanges/BCBreak/MethodBased/ExcludeInternalMethodTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RoaveTest\BackwardCompatibility\DetectChanges\BCBreak\MethodBased;
+
+use PHPUnit\Framework\TestCase;
+use Roave\BackwardCompatibility\Change;
+use Roave\BackwardCompatibility\Changes;
+use Roave\BackwardCompatibility\DetectChanges\BCBreak\MethodBased\ExcludeInternalMethod;
+use Roave\BackwardCompatibility\DetectChanges\BCBreak\MethodBased\MethodBased;
+use Roave\BetterReflection\BetterReflection;
+use Roave\BetterReflection\Reflector\ClassReflector;
+use Roave\BetterReflection\SourceLocator\Type\StringSourceLocator;
+
+/** @covers \Roave\BackwardCompatibility\DetectChanges\BCBreak\MethodBased\ExcludeInternalMethod */
+final class ExcludeInternalMethodTest extends TestCase
+{
+    public function testNormalMethodsAreNotExcluded() : void
+    {
+        $method = (new ClassReflector(new StringSourceLocator(
+            <<<'PHP'
+<?php
+
+class A {
+    function method() {}
+}
+PHP
+            ,
+            (new BetterReflection())->astLocator()
+        )))
+            ->reflect('A')
+            ->getMethod('method');
+
+        $check = $this->createMock(MethodBased::class);
+        $check->expects(self::once())
+              ->method('__invoke')
+              ->with($method, $method)
+              ->willReturn(Changes::fromList(Change::removed('foo', true)));
+
+        self::assertEquals(
+            Changes::fromList(Change::removed('foo', true)),
+            (new ExcludeInternalMethod($check))
+                ->__invoke($method, $method)
+        );
+    }
+
+    public function testInternalFunctionsAreExcluded() : void
+    {
+        $method = (new ClassReflector(new StringSourceLocator(
+            <<<'PHP'
+<?php
+
+class A {
+    /** @internal */
+    function method() {}
+}
+PHP
+            ,
+            (new BetterReflection())->astLocator()
+        )))
+            ->reflect('A')
+            ->getMethod('method');
+
+        $check = $this->createMock(MethodBased::class);
+        $check->expects(self::never())
+              ->method('__invoke');
+
+        self::assertEquals(
+            Changes::empty(),
+            (new ExcludeInternalMethod($check))
+                ->__invoke($method, $method)
+        );
+    }
+}
