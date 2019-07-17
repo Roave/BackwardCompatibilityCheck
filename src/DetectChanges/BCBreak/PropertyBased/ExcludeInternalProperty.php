84e513f9b64b7b519e503d78bc26b874304cfd5b
Marco Pivetta <ocramius@gmail.com>
2019-07-17 09:05:25 +0200

    #51 allow skipping BC checks on internal properties

---
+++ src/DetectChanges/BCBreak/PropertyBased/ExcludeInternalProperty.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\BackwardCompatibility\DetectChanges\BCBreak\PropertyBased;
+
+use Roave\BackwardCompatibility\Changes;
+use Roave\BetterReflection\Reflection\ReflectionProperty;
+use function Safe\preg_match;
+
+final class ExcludeInternalProperty implements PropertyBased
+{
+    /** @var PropertyBased */
+    private $propertyBased;
+
+    public function __construct(PropertyBased $propertyBased)
+    {
+        $this->propertyBased = $propertyBased;
+    }
+
+    public function __invoke(ReflectionProperty $fromProperty, ReflectionProperty $toProperty) : Changes
+    {
+        if ($this->isInternalDocComment($fromProperty->getDocComment())) {
+            return Changes::empty();
+        }
+
+        return $this->propertyBased->__invoke($fromProperty, $toProperty);
+    }
+
+    private function isInternalDocComment(string $comment) : bool
+    {
+        return preg_match('/\s+@internal\s+/', $comment) === 1;
+    }
+}
