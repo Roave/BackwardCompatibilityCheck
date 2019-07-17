d79a5be98f85852aa589f0af47c0e7b1d7d0d2de
Marco Pivetta <ocramius@gmail.com>
2019-07-17 09:03:35 +0200

    #51 allow skipping BC checks on internal interfaces

---
+++ src/DetectChanges/BCBreak/InterfaceBased/ExcludeInternalInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\BackwardCompatibility\DetectChanges\BCBreak\InterfaceBased;
+
+use Roave\BackwardCompatibility\Changes;
+use Roave\BackwardCompatibility\DetectChanges\BCBreak\ClassBased\ExcludeInternalClass;
+use Roave\BetterReflection\Reflection\ReflectionClass;
+use function Safe\preg_match;
+
+/**
+ * Interfaces marked "internal" (docblock) are not affected by BC checks.
+ */
+final class ExcludeInternalInterface implements InterfaceBased
+{
+    /** @var InterfaceBased */
+    private $check;
+
+    public function __construct(InterfaceBased $check)
+    {
+        $this->check = $check;
+    }
+
+    public function __invoke(ReflectionClass $fromInterface, ReflectionClass $toInterface) : Changes
+    {
+        if ($this->isInternalDocComment($fromInterface->getDocComment())) {
+            return Changes::empty();
+        }
+
+        return $this->check->__invoke($fromInterface, $toInterface);
+    }
+
+    private function isInternalDocComment(string $comment) : bool
+    {
+        return preg_match('/\s+@internal\s+/', $comment) === 1;
+    }
+}
