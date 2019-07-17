10ee24dadbceb7800e84a0d06009c85fa6408ff2
Marco Pivetta <ocramius@gmail.com>
2019-07-17 09:05:06 +0200

    #51 allow skipping BC checks on internal traits
    While this is very similar to skipping BC checks on internal classes,
    we need to repeat the declarations due to the monomorphic nature of PHP
    type declarations. If we started to declare Class-alike checks that allow
    for `ClassBased|InterfaceBased|TraitBased` types to be given in, then this
    may be fixed.

---
+++ src/DetectChanges/BCBreak/TraitBased/ExcludeInternalTrait.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\BackwardCompatibility\DetectChanges\BCBreak\TraitBased;
+
+use Roave\BackwardCompatibility\Changes;
+use Roave\BetterReflection\Reflection\ReflectionClass;
+use function Safe\preg_match;
+
+/**
+ * Traits marked "internal" (docblock) are not affected by BC checks.
+ */
+final class ExcludeInternalTrait implements TraitBased
+{
+    /** @var TraitBased */
+    private $check;
+
+    public function __construct(TraitBased $check)
+    {
+        $this->check = $check;
+    }
+
+    public function __invoke(ReflectionClass $fromInterface, ReflectionClass $toInterface) : Changes
+    {
+        if ($this->isInternalDocComment($fromInterface->getDocComment())) {
+            return Changes::empty();
+        }
+
+        return $this->check->__invoke($fromInterface, $toInterface);
+    }
+
+    private function isInternalDocComment(string $comment) : bool
+    {
+        return preg_match('/\s+@internal\s+/', $comment) === 1;
+    }
+}
---
+++ test/unit/DetectChanges/BCBreak/TraitBased/ExcludeInternalTraitTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RoaveTest\BackwardCompatibility\DetectChanges\BCBreak\InterfaceBased;
+
+use PHPUnit\Framework\TestCase;
+use Roave\BackwardCompatibility\Change;
+use Roave\BackwardCompatibility\Changes;
+use Roave\BackwardCompatibility\DetectChanges\BCBreak\TraitBased\ExcludeInternalTrait;
+use Roave\BackwardCompatibility\DetectChanges\BCBreak\TraitBased\TraitBased;
+use Roave\BetterReflection\BetterReflection;
+use Roave\BetterReflection\Reflection\ReflectionClass;
+use Roave\BetterReflection\Reflector\ClassReflector;
+use Roave\BetterReflection\SourceLocator\Type\StringSourceLocator;
+
+/** @covers \Roave\BackwardCompatibility\DetectChanges\BCBreak\TraitBased\ExcludeInternalTrait */
+final class ExcludeInternalTraitTest extends TestCase
+{
+    public function testNormalTraitsAreNotExcluded() : void
+    {
+        $locator    = (new BetterReflection())->astLocator();
+        $reflector  = new ClassReflector(new StringSourceLocator(
+            <<<'PHP'
+<?php
+
+trait ANormalTrait {}
+PHP
+            ,
+            $locator
+        ));
+        $reflection = $reflector->reflect('ANormalTrait');
+
+        $check = $this->createMock(TraitBased::class);
+        $check->expects(self::once())
+              ->method('__invoke')
+              ->with($reflection, $reflection)
+              ->willReturn(Changes::fromList(Change::removed('foo', true)));
+
+        self::assertEquals(
+            Changes::fromList(Change::removed('foo', true)),
+            (new ExcludeInternalTrait($check))
+                ->__invoke($reflection, $reflection)
+        );
+    }
+
+    public function testInternalTraitsAreExcluded() : void
+    {
+        $locator    = (new BetterReflection())->astLocator();
+        $reflector  = new ClassReflector(new StringSourceLocator(
+            <<<'PHP'
+<?php
+
+/** @internal */
+trait AnInternalTrait {}
+PHP
+            ,
+            $locator
+        ));
+        $reflection = $reflector->reflect('AnInternalTrait');
+
+        self::assertInstanceOf(ReflectionClass::class, $reflection);
+
+        $check = $this->createMock(TraitBased::class);
+        $check->expects(self::never())->method('__invoke');
+
+        self::assertEquals(
+            Changes::empty(),
+            (new ExcludeInternalTrait($check))
+                ->__invoke($reflection, $reflection)
+        );
+    }
+}
