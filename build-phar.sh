3985795856428bfb53586b95cecf57572ba131fc
David Arenas <darenas31415@users.noreply.github.com>
2019-09-22 23:51:00 +0100

    #159 Use composer install instead of update

--- build-phar.sh
+++ build-phar.sh
@@ -4,11 +4,9 @@ if [ ! -f box.phar ]; then
     wget https://github.com/humbug/box/releases/download/3.0.0-beta.4/box.phar -O box.phar
 fi
 
-# lock PHP to minimum allowed version
-composer config platform.php 7.2.0
-composer update --no-dev
+# Remove dev dependencies for package distribution
+composer install --no-dev
 
 php box.phar compile
 
-git checkout composer.*
 composer install
