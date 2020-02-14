d140883b2bfc2dcb9e5a6fe540f53970b27db945
Jaapio <jaap@ijaap.nl>
2020-02-14 10:36:24 +0100

    Add test for property visibilty on final classes

--- test/unit/DetectChanges/BCBreak/ClassBased/PropertyChangedTest.php
+++ test/unit/DetectChanges/BCBreak/ClassBased/PropertyChangedTest.php
@@ -80,4 +80,91 @@ PHP
             )
         );
     }
+
+    /**
+     * @dataProvider finalClassProvider
+     */
+    public function testWillIgnoreChangeInFinalClass($fromLocator, $toLocator) : void
+    {
+
+
+        $comparator = $this->createMock(PropertyBased::class);
+
+        $comparator
+            ->expects(self::exactly(2))
+            ->method('__invoke')
+            ->willReturnCallback(static function (ReflectionProperty $from, ReflectionProperty $to) : Changes {
+                $propertyName = $from->getName();
+
+                self::assertSame($propertyName, $to->getName());
+
+                return Changes::fromList(Change::added($propertyName, true));
+            });
+
+        Assertion::assertChangesEqual(
+            Changes::empty(),
+            (new PropertyChanged($comparator))->__invoke(
+                (new ClassReflector($fromLocator))->reflect('TheClass'),
+                (new ClassReflector($toLocator))->reflect('TheClass')
+            )
+        );
+    }
+
+    public function finalClassProvider() : array
+    {
+        $astLocator = (new BetterReflection())->astLocator();
+
+        return [
+            'final class' => [
+                new StringSourceLocator(
+                    <<<'PHP'
+<?php
+
+final class TheClass {
+    protected $b;
+}
+PHP
+                    ,
+                    $astLocator
+                ),
+                new StringSourceLocator(
+                    <<<'PHP'
+<?php
+final class TheClass {
+    private $b;
+}
+PHP
+                    ,
+                    $astLocator
+                ),
+            ],
+            'final class extending baseclass' => [
+                new StringSourceLocator(
+                    <<<'PHP'
+<?php
+abstract class baseClass {}
+
+final class TheClass extends baseClass {
+    protected $b;
+}
+PHP
+                    ,
+                    $astLocator
+                ),
+                new StringSourceLocator(
+                    <<<'PHP'
+<?php
+abstract class baseClass {}
+
+final class TheClass extends baseClass{
+    private $b;
+}
+PHP
+                    ,
+                    $astLocator
+                ),
+            ],
+        ];
+
+    }
 }
