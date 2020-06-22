1806adcfcd0fe8f00080c21b8a4295b475d2c6d7
Marco Pivetta <ocramius@gmail.com>
2020-06-22 13:33:17 +0200

    Moved all builds and tests to github actions

--- build-phar.sh
+++ build-phar.sh
@@ -7,7 +7,7 @@ BOX_DIR="/tmp/box"
 mkdir -p ${BOX_DIR}
 
 # Install humbug/box
-composer --working-dir=${BOX_DIR} require humbug/box "^3.8" --no-interaction --no-progress --no-suggest
+composer --working-dir=${BOX_DIR} require humbug/box "^3.8.4" --no-interaction --no-progress --no-suggest
 
 # Remove dev dependencies for package distribution
 composer install --no-dev
