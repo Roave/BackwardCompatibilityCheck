24c4fe792dea31a2852fb43dfad23ef0379c69f6
Alessandro Lai <alessandro.lai85@gmail.com>
2020-06-22 08:21:59 +0200

    Fix CS

--- src/DetectChanges/BCBreak/PropertyBased/PropertyDocumentedTypeChanged.php
+++ src/DetectChanges/BCBreak/PropertyBased/PropertyDocumentedTypeChanged.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Roave\BackwardCompatibility\DetectChanges\BCBreak\PropertyBased;
 
-use InvalidArgumentException;
 use Roave\BackwardCompatibility\Change;
 use Roave\BackwardCompatibility\Changes;
 use Roave\BackwardCompatibility\Formatter\ReflectionPropertyName;
 use Roave\BetterReflection\Reflection\ReflectionProperty;
-use function array_unique;
 use function implode;
 use function Safe\sort;
 use function Safe\sprintf;
