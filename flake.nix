430bb44a54886351feb8f927a5b239ffc917b0b0
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-12-05 21:04:33 +0100

    simplification by creating a script that build the phar instead

--- flake.nix
+++ flake.nix
@@ -36,51 +36,25 @@
         };
 
         apps = {
-          default = {
+          build-phar = {
             type = "app";
-            program = lib.getExe self'.packages.backwardcompatibilitycheck;
+            program = lib.getExe self'.packages.build-phar-script;
           };
         };
 
         packages = {
-          backwardcompatibilitycheck = php.buildComposerProject {
-            pname = "backwardcompatibilitycheck";
-            version = "8.5.x-dev";
+          build-phar-script = pkgs.writeShellApplication {
+            name = "build-phar-script";
 
-            src = ./.;
-
-            vendorHash = "sha256-QI2wm3m9LTJoeN9g9R3E0BqBQ3PeB81QGSET2LXGKdw=";
-
-            meta.mainProgram = "roave-backward-compatibility-check";
-          };
-
-          phar = pkgs.stdenvNoCC.mkDerivation {
-            pname = "backwardcompatibilitycheck-phar";
-            version = "8.5.x-dev";
-
-            src = ./.;
-
-            buildInputs = [
+            runtimeInputs = [
               php.packages.box
               php.packages.composer
+              pkgs.git
             ];
 
-            buildPhase = ''
-              runHook preBuild
-
-              cp -ar ${self'.packages.backwardcompatibilitycheck}/share/php/backwardcompatibilitycheck/vendor .
+            text = ''
+              composer install --no-dev
               box compile
-
-              runHook postBuild
-            '';
-
-            installPhase = ''
-              runHook preInstall
-
-              mkdir -p $out
-              cp dist/*.phar $out/
-
-              runHook postInstall
             '';
           };
         };
