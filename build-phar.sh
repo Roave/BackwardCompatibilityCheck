a8ffb14ca185b297cc51dffba0125a96846a19a4
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-12-05 17:38:13 +0100

    fix creation of PHAR release

--- build-phar.sh
+++ build-phar.sh
@@ -1,18 +1,17 @@
-#!/usr/bin/env bash
-
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash --pure
+#! nix-shell -p bash phpPackages.box phpPackages.composer
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/fae7198d73783051aae436f84bb91bd9932a6f6d.tar.gz
 set -e
 
 BOX_DIR="/tmp/box"
 
 mkdir -p ${BOX_DIR}
 
-# Install humbug/box
-composer --working-dir=${BOX_DIR} require humbug/box "^3.8" --no-interaction --no-progress --no-suggest
-
 # Remove dev dependencies for package distribution
 composer install --no-dev
 
-${BOX_DIR}/vendor/bin/box compile
+box compile
 
 composer install
 
